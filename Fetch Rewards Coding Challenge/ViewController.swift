7c9516691d29b2a048d3d61f45b74b71405eb136
“jlbeebout” <jlbeebou@ucsc.edu>
2020-01-10 08:41:31 -0800

    Initial Commit

---
+++ Fetch Rewards Coding Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fetch Rewards Coding Challenge
+//
+//  Created by Jonathan Beebout on 1/10/20.
+//  Copyright © 2020 Jonathan Beebout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
